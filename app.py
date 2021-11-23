c468618f073ec0d59ade1a548ac256b378cf5a81
Andy Storey <betterseo@hotmail.co.uk>
2021-11-23 12:09:15 +0000

    Delegates list WIP

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from flask import Flask, render_template
 # create the flask application object
 app = Flask(__name__)
 
+
 # Every page that is on the website will need an app.route defined here
 @app.route('/')
 def get_home():
@@ -26,18 +27,39 @@ def get_acknowledgements():
 
 @app.route('/delegates')
 def get_delegates():
-  #TO DO - Access to the DB, bring the list of delegates into a table.
   strSQL = "SELECT * FROM delegates ORDER BY name ASC"
 
-  return render_template('delegates.html', title='Delegates attending the Convention', description='', data='')
+  # open conntection to the database
+  dbConnection = sqlite3.connect("database.db")
+
+  #Setup a cursor using the current DB connection
+  dbConnection.row_factory = sqlite3.Row
+  cur = dbConnection.cursor()
+  cur.execute(strSQL)
+
+  #Get all of the rows, passing them to the template
+  rows = cur.fetchall(); 
+
+  return render_template('delegates.html', title='Delegates attending the Convention', description='', rows=rows)
 
 @app.route('/delegate/<string:internalURL>')
 def get_delegate(internalURL):
-  #TO DO - Access to the DB, bring the delegate data to each page.
   strSQL = "SELECT * FROM delegates WHERE internalURL = '" + internalURL + "'"
 
+  # open conntection to the database
+  dbConnection = sqlite3.connect("database.db")
+
+  #Setup a cursor using the current DB connection
+  dbConnection.row_factory = sqlite3.Row
+  cur = dbConnection.cursor()
+  cur.execute(strSQL)
+
+  #Get all of the rows, passing them to the template
+  row = cur.fetchone()
+
+
   # In this instane, the meta title and description values must come from the database.
-  return render_template('delegate.html', internalURL=internalURL, title='', description='', data='')
+  return render_template('delegate.html', internalURL=internalURL, title='', description='', row=row)
 
 @app.route('/feedback')
 def get_feedback():
@@ -57,6 +79,11 @@ def get_news():
 @app.errorhandler(404)
 def page_not_found(error):
    return render_template('error404.html', title = 'Page not found'), 404
+
+#@app.teardown_appcontext
+#def closeDB():
+#  dbConnection.close
+#  return True
 
 # start the server with the 'run()' method - debug=True for testing - NOT LIVE
 if __name__ == '__main__':
